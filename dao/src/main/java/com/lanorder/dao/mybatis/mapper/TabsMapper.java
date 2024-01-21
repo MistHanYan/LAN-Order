66a3b3124d2af98c10dfc493424db6c5d423b48c
MistHanYan <129029870+MistHanYan@users.noreply.github.com>
2024-01-21 17:51:04 +0800

    Delete dao directory

--- dao/src/main/java/com/lanorder/dao/mybatis/mapper/TabsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.lanorder.dao.mybatis.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.lanorder.common.entity.pojo.Tabs;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TabsMapper extends BaseMapper<Tabs> {
-}
